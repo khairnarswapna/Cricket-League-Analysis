662ea776d675316201689dac6dad732ba5839be1
khairnarswapna <khairnarswapna99@gmail.com>
2019-12-14 15:37:54 +0530

    Added:add testcase for given IPL Analyser Data With Incorrect Header ShouldT hrow Exception

--- src/test/java/cricketiplanalyser/IplCricketAnalyserTest.java
+++ src/test/java/cricketiplanalyser/IplCricketAnalyserTest.java
@@ -1,7 +1,5 @@
 package cricketiplanalyser;
 
-import censusanalyser.CensusAnalyser;
-import censusanalyser.CensusAnalyserException;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -10,6 +8,8 @@ public class IplCricketAnalyserTest {
 
     private static final String IPL2019_RUNS_CSV_FILE_PATH ="./src/test/resources/IPL2019FactsheetMostRuns.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+    private static final String DELIMETER_MISSING_IPL_MOST_RUN_FILE="./src/test/resources/delimeterMissingIPL2019MostRuns.csv";
+    private static final String HEADER_MISSING_IPL_MOST_RUN_FILE="./src/test/resources/HeaderMissingIPL2019MostRun.csv";
     @Test
     public void givenLeagueMostRunDataCSVFile_ShouldReturnExactCount() {
         try {
@@ -27,10 +27,35 @@ public class IplCricketAnalyserTest {
         try {
             CricketAnalyser cricketAnalyser = new CricketAnalyser();
             ExpectedException exceptionRule = ExpectedException.none();
-            exceptionRule.expect(CensusAnalyserException.class);
+            exceptionRule.expect(CricketAnalyserException.class);
             cricketAnalyser.loadCSVData(WRONG_CSV_FILE_PATH);
         } catch (CricketAnalyserException e) {
             Assert.assertEquals(CricketAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
+
+    @Test
+    public void givenIPLAnalyserData_WithIncorrectDelimiter_ShouldThrowException() {
+        try {
+            CricketAnalyser cricketAnalyser = new CricketAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CricketAnalyserException.class);
+            cricketAnalyser.loadCSVData(DELIMETER_MISSING_IPL_MOST_RUN_FILE);
+        } catch (CricketAnalyserException e) {
+            System.out.println();
+            Assert.assertEquals(CricketAnalyserException.ExceptionType.SOME_FILE_ISSUE, e.type);
+        }
+    }
+
+    @Test
+    public void givenIPLAnalyserData_WithIncorrectHeader_ShouldThrowException() {
+        try {
+            CricketAnalyser cricketAnalyser = new CricketAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CricketAnalyserException.class);
+            cricketAnalyser.loadCSVData(HEADER_MISSING_IPL_MOST_RUN_FILE);
+        } catch (CricketAnalyserException e) {
+            Assert.assertEquals(CricketAnalyserException.ExceptionType.SOME_FILE_ISSUE, e.type);
         }
     }
 
