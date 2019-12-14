837b85c536b195f24e718f12a88b315bf1f95616
khairnarswapna <khairnarswapna99@gmail.com>
2019-12-14 23:32:39 +0530

    Refactor:used stream and applied csvbuilder jar file while loading CSV data

--- src/main/java/cricketiplanalyser/CricketAnalyser.java
+++ src/main/java/cricketiplanalyser/CricketAnalyser.java
@@ -1,6 +1,7 @@
 package cricketiplanalyser;
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
+import CSVBuilder.CSVBuilderException;
+import CSVBuilder.CSVBuilderFactory;
+import CSVBuilder.ICSVBuilder;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -15,20 +16,20 @@ public class CricketAnalyser {
     public int loadCSVData(String csvFilePath) throws CricketAnalyserException {
         try {
             Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
-            CsvToBeanBuilder<BatsManRunCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-            csvToBeanBuilder.withType(BatsManRunCSV.class);
-            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<BatsManRunCSV> csvToBean = csvToBeanBuilder.build();
-            Iterator<BatsManRunCSV> censusCSVIterator = csvToBean.iterator();
-            Iterable<BatsManRunCSV> csvIterable=()->censusCSVIterator;
-            int numOfEntries=(int) StreamSupport.stream(csvIterable.spliterator(),false).count();
-            return numOfEntries;
+            ICSVBuilder csvBuilder= CSVBuilderFactory.createCSVbuilder();
+            Iterator<BatsManRunCSV> iplCSVIterator = csvBuilder.getCSVFileItrator(reader, BatsManRunCSV.class);
+            Iterable<BatsManRunCSV> csvIterable = () -> iplCSVIterator;
+            int CricketerCount = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
+            return CricketerCount;
         } catch (IOException e) {
             throw new CricketAnalyserException(e.getMessage(),
                     CricketAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         }catch(RuntimeException e){
             throw new CricketAnalyserException(e.getMessage(),CricketAnalyserException.ExceptionType.SOME_FILE_ISSUE);
+        } catch (CSVBuilderException e) {
+            e.printStackTrace();
         }
+        return 0;
     }
 
 }
