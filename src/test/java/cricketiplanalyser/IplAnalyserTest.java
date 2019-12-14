d6a283f6a98ab55b6830de1fffae0518fdf13746
khairnarswapna <khairnarswapna99@gmail.com>
2019-12-14 13:14:00 +0530

    Refacor:renaming the method name

--- src/test/java/cricketiplanalyser/IplAnalyserTest.java
+++ src/test/java/cricketiplanalyser/IplAnalyserTest.java
@@ -8,9 +8,14 @@ public class IplAnalyserTest {
     private static final String IPL2019_RUNS_CSV_FILE_PATH ="./src/test/resources/IPL2019FactsheetMostRuns.csv";
     @Test
     public void givenLeagueMostRunDataCSVFile_ShouldReturnExactCount() {
-        CricketAnalyser cricketAnalyser = new CricketAnalyser();
-        int totalRecord = cricketAnalyser.readCSVData(IPL2019_RUNS_CSV_FILE_PATH);
-        Assert.assertEquals(101,totalRecord);
+        try {
+            CricketAnalyser cricketAnalyser = new CricketAnalyser();
+            int totalRecord = cricketAnalyser.loadCSVData(IPL2019_RUNS_CSV_FILE_PATH);
+            System.out.println(totalRecord);
+            Assert.assertEquals(101,totalRecord);
+        } catch (CricketAnalyserException e) {
+            e.printStackTrace();
+        }
     }
 
 }
