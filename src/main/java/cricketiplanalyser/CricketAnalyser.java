ec4ba44383a21a99498eca1786e447d2273c6458
khairnarswapna <khairnarswapna99@gmail.com>
2019-12-15 14:35:44 +0530

    Added: enum added for SortByField of BatsManMostRunCsv

--- src/main/java/cricketiplanalyser/CricketAnalyser.java
+++ src/main/java/cricketiplanalyser/CricketAnalyser.java
@@ -2,34 +2,47 @@ package cricketiplanalyser;
 import CSVBuilder.CSVBuilderException;
 import CSVBuilder.CSVBuilderFactory;
 import CSVBuilder.ICSVBuilder;
+import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 public class CricketAnalyser {
 
-    public int loadCSVData(String csvFilePath) throws CricketAnalyserException {
-        try {
-            Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+    Map<String,BatsManRunCSV> iplRunsCsvMap = new HashMap<>();
+    public CricketAnalyser() {
+        this.iplRunsCsvMap = new HashMap<>();
+    }
+    public Map loadIPLCSVData(String csvFilePath) throws CricketAnalyserException {
+        try ( Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));){
             ICSVBuilder csvBuilder= CSVBuilderFactory.createCSVbuilder();
             Iterator<BatsManRunCSV> iplCSVIterator = csvBuilder.getCSVFileItrator(reader, BatsManRunCSV.class);
             Iterable<BatsManRunCSV> csvIterable = () -> iplCSVIterator;
-            int CricketerCount = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
-            return CricketerCount;
+            StreamSupport.stream(csvIterable.spliterator(),false)
+                    .map(BatsManRunCSV.class::cast)
+                    .forEach(iplRuns -> this.iplRunsCsvMap.put(iplRuns.playerName, iplRuns));
+            return iplRunsCsvMap;
         } catch (IOException e) {
             throw new CricketAnalyserException(e.getMessage(),
                     CricketAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         }catch(RuntimeException e){
             throw new CricketAnalyserException(e.getMessage(),CricketAnalyserException.ExceptionType.SOME_FILE_ISSUE);
         } catch (CSVBuilderException e) {
-            e.printStackTrace();
+            throw new CricketAnalyserException(e.getMessage(),CricketAnalyserException.ExceptionType.SOME_FILE_ISSUE);
         }
-        return 0;
+    }
+    public String sortbyFields(SortbyField.Field feild) {
+        Comparator<BatsManRunCSV> iplComparator = new SortbyField().getParameter(feild);
+        ArrayList<BatsManRunCSV> list = this.iplRunsCsvMap.values().stream()
+                .sorted(iplComparator)
+                .collect(Collectors.toCollection(ArrayList::new));
+        String sortIplToJson = new Gson().toJson(list);
+        return sortIplToJson;
     }
 
 }
--- src/main/java/cricketiplanalyser/SortbyField.java
+++ src/main/java/cricketiplanalyser/SortbyField.java
@@ -1,4 +1,20 @@
 package cricketiplanalyser;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 public class SortbyField {
+
+    public Map<Field, Comparator> sortByFields = new HashMap<>();
+    public enum Field {
+        AVERAGE
+    }
+    public SortbyField() {
+        sortByFields.put( Field.AVERAGE, Comparator.<BatsManRunCSV,Double>comparing(census -> (census.average),Comparator.reverseOrder()));
+    }
+
+    public Comparator getParameter( Field field) {
+        Comparator<BatsManRunCSV> comparator = sortByFields.get(field);
+        return comparator;
+    }
 }
